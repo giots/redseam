e6e0439fa7ac0c3378da399b222ad13f971d6f01
Giorgi Tsartsidze <george.tsartsidze@gmail.com>
2025-09-24 15:11:16 +0400

    initial html and css part of the header

--- include/header.php
+++ include/header.php
@@ -12,23 +12,22 @@
 
 
         <link href="css/main.css" rel="stylesheet" >
-        <!--
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-        -->
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
     </head>
     <body>
 
 
     <header class='header'>
-        <div>
+        <div class='left_part'> 
             <div class="logo"><img src='/assets/logo.svg'></div>
             <div class="title">RedSeam Clothing </div>
         </div>
-        <div>
-            <div class="logo"><img src='/assets/logo.svg'></div>
-            <div class="title">RedSeam Clothing </div>
+        <div class='right_part'>
+            <div class="avatar"><img src='/assets/avatar.svg'></div>
+            <div class="login_title"><a href=''>login</a> </div>
         </div>
     </header>
  
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php 
 
+    # inclulde Header part
     include('include/header.php');
 
+    #include main part
     include('include/home.php');
     
+    #include footer part
     include('include/footer.php');
 
 ?>
